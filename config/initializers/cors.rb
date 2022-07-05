b52aac5791988d5905b62c03afc32211b921236a
ibtechraj <krs30018@gmail.com>
2022-07-05 21:57:36 +0530

    fixing cors error

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://mymotorwash.herokuapp.com:3000/'
+    origins 'http://localhost:3000/'
     # origins '*'
 
     resource(
