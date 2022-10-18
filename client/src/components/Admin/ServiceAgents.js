7e78b93f1b489c16aac535c655406c8c16d3c56b
ibtechraj <krs30018@gmail.com>
2022-10-18 21:20:36 +0530

    Password reset link email

--- client/src/components/Admin/ServiceAgents.js
+++ client/src/components/Admin/ServiceAgents.js
@@ -1,5 +1,5 @@
-import React from 'react'
-
+import React, { useState, useEffect} from 'react'
+import axios from 'axios';
 export default function ServiceAgents() {
 
   const [agentsData, setAgentsData] = useState(null);
